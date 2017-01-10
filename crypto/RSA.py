38a252d4162dba262b690bff99905e295017fb22
Devernua <kukuev@phystech.edu>
2017-01-10 20:49:02 +0300

    add chinese_remainder

--- crypto/RSA.py
+++ crypto/RSA.py
@@ -1,3 +1,4 @@
+from functools import reduce
 
 
 def mul_inv(a, n):
@@ -6,7 +7,7 @@ def mul_inv(a, n):
 
     :param a: digit
     :param n: prime
-    :return: a^(-1) (mod(b))
+    :return: a**(-1) (mod(b))
     """
 
     b0 = n
@@ -24,3 +25,19 @@ def mul_inv(a, n):
         x1 += b0
 
     return x1
+
+
+def chinese_remainder(pares):
+    """
+    Realise of Chinese remainder theorem
+    :param pares: list of (c,n) where c is digit and n is prime
+    :return: m**(len(pares)) where c = m mod n for each pares
+    """
+
+    sum = 0
+    prod = reduce(lambda a, b: a * b, [i[1] for i in pares])
+
+    for a_i, n_i in pares:
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
